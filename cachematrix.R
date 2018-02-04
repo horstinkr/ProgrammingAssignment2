8f04cba48bb52d79662dc18bf8f963389a4dfc7b
horstinkr <36136543+horstinkr@users.noreply.github.com>
2018-02-04 21:34:05 +0000

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Programming Assignment 2 for Reinoud Horstink - reinoud.horstink@arcadis.com
+## Data science course, Programming R (week 3)
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	## This function creates a special "matrix" object that can cache its inverse
+	## Assume that the matrix supplied is always invertible
+	
+	# actually, I've chosen to use a dataframe instead of a matrix
+	# use <<- to bring this cache object to the global environment
+
+	# xser: x serialized (as reference key)
+	# xi:   inverse matrix of x
+	
+	mycachematrix <<- data.frame(xser="a", xi="b")
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	## Return a matrix that is the inverse of 'x'
+	## If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve should retrieve the inverse from the cache.
+	## xi: inverse of matrix x
+
+	# serialize the original matrix as unique key for looking up the inverse
+	xser <- serialize(x, NULL)
+
+	if (!is.na(mycachematrix$xser[[xser]])) {
+		# inverse does already exist in cache --> lookup inverse matrix in cache object
+		xi <- mycachematrix$xser[[xser]]
+	} else {
+		# inverse does not yet exist in cache --> compute inverse matrix now and store it in mycachematrix
+		xi <- solve(x, ...)
+		mycachematrix[[xser]] <<- xi
+	}
+	
+	# return inverse matrix
+	xi
+
 }
